26f42c0cdddf9958783b19cd4e0b971abfc88655
Elias <Elias@DESKTOP-VVQP9VR>
2018-03-08 12:17:23 -0300

    Indicadores buscando via CURL informações de pais, sede e area

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -5,8 +5,23 @@ use Core\BaseController;
 
 class HomeController extends BaseController
 {
+    public function __construct()
+    {
+        session_start();
+    }
+    
     public function index()
     {
+        //Assume o Token Gerado no Login, em desenvolvimento local usa um token fixo
+        if($_SERVER['SERVER_NAME'] != 'localhost')
+        {
+            $_SESSION['Planificacion']['token'] = $_GET['token'];
+        }
+        else
+        {
+            $_SESSION['Planificacion']['token'] = '7c9b5c9b9baae1227deb96f1c51a7b61';
+        }
+        
         $this->setPageTitle('Home');
         $this->renderView('home/index', 'layout');
     }
--- app/Controllers/IndicadoresController.php
+++ app/Controllers/IndicadoresController.php
@@ -6,13 +6,111 @@ use Core\Container;
 
 class IndicadoresController extends BaseController
 {
+    public function __construct()
+    {
+        session_start();
+    }
+    
     public function index()
     {
         $this->setPageTitle('Indicadores');
         $model = Container::getModel("Indicador");
         $this->view->indicador = $model->select();
-               
-        /* Render View Paises */
+        $aKPI = $this->view->indicador;
+        
+        for($i=0; $i < count($aKPI); $i++)
+        {
+            $aKPI[$i] = (array) $aKPI[$i];
+            
+            //Get Area
+            $area = $this->GetArea($aKPI[$i]['id_area']);
+            $aKPI[$i]['area'] = $area['nombre'];
+            
+            //Get Pais
+            if($aKPI[$i]['id_pais'])
+            {
+                $pais = $this->GetPais($aKPI[$i]['id_pais']);
+                $aKPI[$i]['pais'] = $pais['nombre'];
+            }
+            else 
+            {
+                $aKPI[$i]['pais'] = 'Todos';
+            }
+            
+            //Get Sede
+            if($aKPI[$i]['id_sede'])
+            {
+                $sede = $this->GetSede($aKPI[$i]['id_sede']);
+                $aKPI[$i]['sede'] = $sede['nombre'];
+            }
+            else
+            {
+                $aKPI[$i]['sede'] = 'Todas';
+            }
+            
+            $this->view->indicador[$i] = (object) $aKPI[$i];
+            
+        }
+        
+        /* Render View KPIs */
         $this->renderView('indicadores/index', 'layout');
+    }
+    
+    //Busca Area en id.techo.org
+    public function GetArea($idArea)
+    {
+        $url = 'http://id.techo.org/area?api=true&token='.$_SESSION['Planificacion']['token'].'&id='.$idArea;
+        
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_CAINFO, getcwd() . DIRECTORY_SEPARATOR . 'cacert.pem');
+        
+        $output = curl_exec($curl);
+        curl_close($curl);
+        
+        $data = json_decode($output, true);
+        
+        return $data;
+    }
+    
+    //Busca Pais en id.techo.org
+    public function GetPais($idPais)
+    {
+        $url = 'http://id.techo.org/pais?api=true&token='.$_SESSION['Planificacion']['token'].'&id='.$idPais;
+        
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        
+        $output = curl_exec($curl);
+        curl_close($curl);
+        
+        $data = json_decode($output, true);
+        
+        return $data;
+    }
+    
+    //Busca Sede en id.techo.org
+    public function GetSede($idSede)
+    {
+        $url = 'http://id.techo.org/sede?api=true&token='.$_SESSION['Planificacion']['token'].'&id='.$idSede;
+        
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        
+        $output = curl_exec($curl);
+        curl_close($curl);
+        
+        $data = json_decode($output, true);
+        
+        return $data;
     }
 }
