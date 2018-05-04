c43acf0fd50f6a66cfe4428147779dfcf6ac7671
Elias <Elias@DESKTOP-VVQP9VR>
2018-05-04 17:57:34 -0300

    listado de pais informe trimestral por pais y sede

--- app/Controllers/TInformeController.php
+++ app/Controllers/TInformeController.php
@@ -21,8 +21,44 @@ class TInformeController extends BaseController
         $this->setPageTitle('Informe Trimestre');
         $model = Container::getModel("CPlanificacion");
         
+        //Busca Anos
+        $this->view->ano = $model->ListaAno();
+        
+        //Todos Paises
+        $paises = $this->Paises();
+        
+        unset($paises[15]);
+        
+        $this->view->paises = $paises;
+        
         /* Render View Paises */
         $this->renderView('tinforme/index', 'layout');
+    }
+    
+    public function Paises()
+    {
+        $url = 'http://id.techo.org/pais?api=true&token='.$_SESSION['Planificacion']['token'];
+        
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_CAINFO, getcwd() . DIRECTORY_SEPARATOR . 'cacert.pem');
+        
+        $output = curl_exec($curl);
+        curl_close($curl);
+        
+        $data = json_decode($output, true);
+        
+        for($i=0; $i < count($data); $i++)
+        {
+            $aTemp[$i]['id']   = $data[$i]['ID_Pais'];
+            $aTemp[$i]['pais'] = $data[$i]['Nombre_Pais'];
+        }
+        
+        //  echo json_encode(array("values" => $aTemp));
+        return $aTemp;
     }
     
 }
