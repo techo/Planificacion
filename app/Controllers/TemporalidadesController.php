b7607ad8ea6e0bbb83aeca40e66f7aac7006584f
Elias <Elias@172.16.1.81>
2018-06-08 17:10:13 -0400

    Redirecionamento admin.planificacion.org e planificacion.techo.org

--- app/Controllers/TemporalidadesController.php
+++ app/Controllers/TemporalidadesController.php
@@ -10,9 +10,19 @@ class TemporalidadesController extends BaseController
     {
         session_start();
         
-        if(!isset($_SESSION['Planificacion']['token']))
+        if($_SERVER['SERVER_NAME'] == 'admin.planificacion.techo.org' || $_SERVER['SERVER_NAME'] == 'localhost')
         {
-            header('Location: http://login.techo.org/?appid=245sd4d5f4g8h1rt4584ht84t54tg8tg&redirect=https://planificacion.techo.org/');
+            if(!isset($_SESSION['Planificacion']['token']))
+            {
+                header('Location: http://login.techo.org/?appid=98532jvfn145sas87aawrh154aeeth&redirect=https://admin.planificacion.techo.org/');
+            }
+        }
+        else
+        {
+            if(!isset($_SESSION['Planificacion']['token']))
+            {
+                header('Location: http://login.techo.org/?appid=245sd4d5f4g8h1rt4584ht84t54tg8tg&redirect=https://planificacion.techo.org/');
+            }
         }
     }
     
