82d07ad45e3190531246edc3e26a5cc7b77f3ad7
Elias Lima <eliasv.lima@yahoo.com.br>
2020-02-05 17:47:24 -0200

    eliminado opciones export exce, pdf y print

--- public/assets/js/tinformes/monitoreo.js
+++ public/assets/js/tinformes/monitoreo.js
@@ -88,20 +88,7 @@ $("#loadSede").change(function()
 				   	        dom: '<"html5buttons"B>lTfgitp',
 				   	        buttons: [
 				   	            { extend: 'copy'},
-				   	            {extend: 'excel', title: 'ExampleFile'},
-				   	            {extend: 'pdf', title: 'ExampleFile'},
-
-				   	            {extend: 'print',
-				   	             customize: function (win){
-				   	                    $(win.document.body).addClass('white-bg');
-				   	                    $(win.document.body).css('font-size', '10px');
-
-				   	                    $(win.document.body).find('table')
-				   	                            .addClass('compact')
-				   	                            .css('font-size', 'inherit');
-				   	            }
-				   	            }
-				   	        ]
+				   	            ]
 
 				   	    });
 				}
